419f155ce0b51704b2706ce7e0fb94023ff3a939
Mukesh Sharma <mukeshsharma24@gmail.com>
2021-08-05 09:18:22 +1200

    Added tgw resource

---
+++ centralized/transit_gateway.tf
@@ -0,0 +1,10 @@
+resource "aws_ec2_transit_gateway" "tgw" {
+  description     = "development-transit-gateway"
+  amazon_side_asn = var.amazon_side_asn
+
+  auto_accept_shared_attachments  = "enable"
+  default_route_table_propagation = "enable"
+  default_route_table_association = "disable"
+
+
+}
