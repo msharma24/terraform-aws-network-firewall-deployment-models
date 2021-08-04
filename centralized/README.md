b7a92cfd1cf373843e1e371894c5c210944b3976
Mukesh Sharma <mukeshsharma24@gmail.com>
2021-08-05 11:02:06 +1200

    Updated Spoke Attachment Configurations

---
+++ centralized/README.md
@@ -0,0 +1,9 @@
+# Centralized AWS Network Firewall Deployment Model
+
+## Description
+
+
+
+
+#### Notes:
+[Appliance Mode Enabled on the Firewall Inspection VPC](https://aws.amazon.com/blogs/networking-and-content-delivery/centralized-inspection-architecture-with-aws-gateway-load-balancer-and-aws-transit-gateway/)
--- centralized/transit_gateway.tf
+++ centralized/transit_gateway.tf
@@ -2,15 +2,83 @@ resource "aws_ec2_transit_gateway" "tgw" {
   description     = "development-transit-gateway"
   amazon_side_asn = var.amazon_side_asn
 
-  auto_accept_shared_attachments  = "enable"
+  auto_accept_shared_attachments = "enable"
+
   default_route_table_propagation = "enable"
   default_route_table_association = "disable"
+
+  vpn_ecmp_support = "enable"
+  dns_support      = "enable"
 
   tags = {
     Name      = "development-transit-gateway"
     ManagedBy = "Terraform"
 
   }
+
+
+}
+
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "spoke_vpc_a_tgw_attachment" {
+  vpc_id                                          = module.spoke_vpc_a.vpc_id
+  transit_gateway_id                              = aws_ec2_transit_gateway.tgw.id
+  subnet_ids                                      = module.spoke_vpc_a.private_subnets
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = true
+
+  tags = {
+    Name      = "spoke_vpc_a_tgw_attachment"
+    ManagedBy = "Terraform"
+
+  }
+
+
+
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "spoke_vpc_b_tgw_attachment" {
+  vpc_id                                          = module.spoke_vpc_b.vpc_id
+  transit_gateway_id                              = aws_ec2_transit_gateway.tgw.id
+  subnet_ids                                      = module.spoke_vpc_b.private_subnets
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = true
+
+  tags = {
+    Name      = "spoke_vpc_b_tgw_attachment"
+    ManagedBy = "Terraform"
+
+  }
+
+
+
+}
+
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "inspection_vpc_tgw_attachment" {
+  vpc_id             = aws_vpc.inspection_vpc.id
+  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+  subnet_ids = [
+    aws_subnet.inspection_vpc_tgw_subnet_a.id,
+    aws_subnet.inspection_vpc_tgw_subnet_b.id,
+    aws_subnet.inspection_vpc_tgw_subnet_c.id
+  ]
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = true
+
+  /*
+   * Appliance Mode Enabled on the Firewall Inspection VPC
+   * See Notes section in README.md
+   */
+
+  appliance_mode_support = "enable"
+
+  tags = {
+    Name      = "inspection_vpc_tgw_attachment"
+    ManagedBy = "Terraform"
+
+  }
+
 
 
 }
