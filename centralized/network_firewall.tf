d4d98776cc2730879deb4e860bba26ec26da5cde
Mukesh Sharma <mukeshsharma24@gmail.com>
2021-08-11 09:05:16 +1200

    Added fw logging and a new ec2

--- centralized/network_firewall.tf
+++ centralized/network_firewall.tf
@@ -38,3 +38,60 @@ resource "aws_networkfirewall_firewall" "nfw" {
   }
 
 }
+
+# Logging configuration
+resource "aws_cloudwatch_log_group" "anfw_alert_log_group" {
+  name = "/aws/network-firewall/alert"
+}
+
+resource "random_string" "bucket_random_id" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+resource "aws_s3_bucket" "anfw_flow_bucket" {
+  bucket        = "network-firewall-flow-bucket-${random_string.bucket_random_id.id}"
+  acl           = "private"
+  force_destroy = true
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "aws/s3"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "anfw_flow_bucket_public_access_block" {
+  bucket = aws_s3_bucket.anfw_flow_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_networkfirewall_logging_configuration" "anfw_alert_logging_configuration" {
+  firewall_arn = aws_networkfirewall_firewall.nfw.arn
+  logging_configuration {
+    log_destination_config {
+      log_destination = {
+        logGroup = aws_cloudwatch_log_group.anfw_alert_log_group.name
+      }
+      log_destination_type = "CloudWatchLogs"
+      log_type             = "ALERT"
+    }
+    log_destination_config {
+      log_destination = {
+        bucketName = aws_s3_bucket.anfw_flow_bucket.bucket
+      }
+      log_destination_type = "S3"
+      log_type             = "FLOW"
+    }
+  }
+}
