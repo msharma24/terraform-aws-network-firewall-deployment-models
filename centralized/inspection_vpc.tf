a070e1b54ee680ab49c6e14ddddeb03df13f5a1a
Mukesh Sharma <mukeshsharma24@gmail.com>
2021-08-11 08:51:02 +1200

    Fixed inspection vpc route tables

--- centralized/inspection_vpc.tf
+++ centralized/inspection_vpc.tf
@@ -86,7 +86,7 @@ resource "aws_subnet" "inspection_vpc_tgw_subnet_c" {
 #---------------------------------------------------------------------------
 # Firewall Route Table
 #---------------------------------------------------------------------------
-resource "aws_route_table" "inspection_vpc_firewall_rt" {
+resource "aws_route_table" "inspection_vpc_firewall_rt_1" {
   vpc_id = aws_vpc.inspection_vpc.id
 
   route {
@@ -99,12 +99,12 @@ resource "aws_route_table" "inspection_vpc_firewall_rt" {
   ]
 
   tags = {
-    Name = "inspection-vpc/firewall-route-table"
+    Name = "inspection-vpc/firewall-route-table_1"
   }
 
 }
 
-resource "aws_route_table" "inspection_vpc_tgw_rt_2" {
+resource "aws_route_table" "inspection_vpc_firewall_rt_2" {
   vpc_id = aws_vpc.inspection_vpc.id
 
   route {
@@ -122,7 +122,7 @@ resource "aws_route_table" "inspection_vpc_tgw_rt_2" {
 
 }
 
-resource "aws_route_table" "inspection_vpc_tgw_rt_3" {
+resource "aws_route_table" "inspection_vpc_firewall_rt_3" {
   vpc_id = aws_vpc.inspection_vpc.id
   route {
     cidr_block      = "0.0.0.0/0"
@@ -141,27 +141,27 @@ resource "aws_route_table" "inspection_vpc_tgw_rt_3" {
 
 resource "aws_route_table_association" "firewall_subnet_association_a" {
   subnet_id      = aws_subnet.inspection_vpc_firewall_subnet_a.id
-  route_table_id = aws_route_table.inspection_vpc_firewall_rt.id
+  route_table_id = aws_route_table.inspection_vpc_firewall_rt_1.id
 
 }
 
 resource "aws_route_table_association" "firewall_subnet_association_b" {
   subnet_id      = aws_subnet.inspection_vpc_firewall_subnet_b.id
-  route_table_id = aws_route_table.inspection_vpc_firewall_rt.id
+  route_table_id = aws_route_table.inspection_vpc_firewall_rt_2.id
 
 }
 
 
 resource "aws_route_table_association" "firewall_subnet_association_c" {
   subnet_id      = aws_subnet.inspection_vpc_firewall_subnet_c.id
-  route_table_id = aws_route_table.inspection_vpc_firewall_rt.id
+  route_table_id = aws_route_table.inspection_vpc_firewall_rt_3.id
 
 }
 
 #---------------------------------------------------------------------------
 # Transit Gateway  Route Table
 #---------------------------------------------------------------------------
-resource "aws_route_table" "inspection_vpc_tgw_rt" {
+resource "aws_route_table" "inspection_vpc_tgw_rt_1" {
   vpc_id = aws_vpc.inspection_vpc.id
 
   route {
@@ -169,33 +169,72 @@ resource "aws_route_table" "inspection_vpc_tgw_rt" {
     transit_gateway_id = aws_ec2_transit_gateway.tgw.id
   }
 
-  # depends_on = [
-  #   module.tgw.ec2_transit_gateway_id
-  # ]
+  depends_on = [
+    aws_ec2_transit_gateway.tgw
+  ]
 
 
   tags = {
-    Name = "inspection-vpc/tgw-route-table"
+    Name = "inspection-vpc/tgw-route-table-1"
   }
 
 }
 
+resource "aws_route_table" "inspection_vpc_tgw_rt_2" {
+  vpc_id = aws_vpc.inspection_vpc.id
+
+  route {
+    cidr_block         = "0.0.0.0/0"
+    transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+  }
+
+  depends_on = [
+    aws_ec2_transit_gateway.tgw
+  ]
+
+
+  tags = {
+    Name = "inspection-vpc/tgw-route-table_2"
+  }
+
+}
+
+resource "aws_route_table" "inspection_vpc_tgw_rt_3" {
+  vpc_id = aws_vpc.inspection_vpc.id
+
+  route {
+    cidr_block         = "0.0.0.0/0"
+    transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+  }
+
+  depends_on = [
+    aws_ec2_transit_gateway.tgw
+  ]
+
+
+  tags = {
+    Name = "inspection-vpc/tgw-route-table_3"
+  }
+
+}
+
+
 resource "aws_route_table_association" "tgw_subnet_association_a" {
   subnet_id      = aws_subnet.inspection_vpc_tgw_subnet_a.id
-  route_table_id = aws_route_table.inspection_vpc_tgw_rt.id
+  route_table_id = aws_route_table.inspection_vpc_tgw_rt_1.id
 
 }
 
 resource "aws_route_table_association" "tgw_subnet_association_b" {
   subnet_id      = aws_subnet.inspection_vpc_tgw_subnet_b.id
-  route_table_id = aws_route_table.inspection_vpc_tgw_rt.id
+  route_table_id = aws_route_table.inspection_vpc_tgw_rt_2.id
 
 }
 
 
 resource "aws_route_table_association" "tgw_subnet_association_c" {
   subnet_id      = aws_subnet.inspection_vpc_tgw_subnet_c.id
-  route_table_id = aws_route_table.inspection_vpc_tgw_rt.id
+  route_table_id = aws_route_table.inspection_vpc_tgw_rt_3.id
 
 }
 
