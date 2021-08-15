6d6d6437d38facdd1fc24bd421daaa6e5186dd9b
Mukesh Sharma <mukeshsharma24@gmail.com>
2021-08-16 08:40:09 +1200

    added test instances config

--- distributed/iam.tf
+++ distributed/iam.tf
@@ -67,3 +67,40 @@ resource "aws_iam_instance_profile" "malicious_instance_iam_profile" {
   role = aws_iam_role.malicious_instance_iam_role.name
 
 }
+#--------------------------------------------------------------------------
+# Subnet IAM Role
+#--------------------------------------------------------------------------
+resource "aws_iam_role" "subnet_role" {
+  name = "subnet_role-${random_id.random_id.hex}"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "policy_attach" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+  role       = aws_iam_role.subnet_role.name
+}
+
+resource "aws_iam_instance_profile" "subnet_instance_iam_profile" {
+  name = "SubnetInstanceProfile-${random_id.random_id.hex}"
+  role = aws_iam_role.subnet_role.name
+
+}
---
+++ distributed/test-ec2.tf
@@ -0,0 +1,56 @@
+resource "aws_security_group" "subnet_security_group" {
+  name        = "test-sg-1-${random_id.random_id.hex}"
+  description = "Allow instances to get to SSM Systems Manager"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress = [
+    {
+      description      = "Test Instance SG"
+      from_port        = "-1"
+      to_port          = "-1"
+      protocol         = "icmp"
+      ipv6_cidr_blocks = ["::/0"]
+      cidr_blocks      = ["10.0.0.0/8"]
+      self             = false
+      security_groups  = []
+      prefix_list_ids  = []
+    }
+  ]
+
+
+
+  tags = {
+    Name = "test-sg-1-${random_id.random_id.hex}"
+  }
+
+}
+
+data "template_file" "test_instance_userdata" {
+  template = file("${path.cwd}/userdata-script/test_instance_script.sh")
+  vars = {
+    MaliciousIP = aws_eip.malicious_instance_eip.id
+  }
+}
+
+resource "aws_instance" "test_instance_1" {
+  ami             = data.aws_ami.amazon-linux-2.id
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.private_subnet_1.id
+  security_groups = [aws_security_group.subnet_security_group.id]
+  user_data       = data.template_file.test_instance_userdata.rendered
+
+  tags = {
+    Name = "test-instance-1"
+  }
+}
+
+resource "aws_instance" "test_instance_2" {
+  ami             = data.aws_ami.amazon-linux-2.id
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.private_subnet_1.id
+  security_groups = [aws_security_group.subnet_security_group.id]
+
+  tags = {
+    Name = "test-instance-2"
+  }
+}
