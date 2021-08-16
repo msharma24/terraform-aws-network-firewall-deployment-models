38caa2232bb40a8c0d30611a4ee3c72646a72dd9
Mukesh Sharma <mukeshsharma24@gmail.com>
2021-08-16 14:56:07 +1200

    deleted keypair resource

--- centralized/ec2_instances.tf
+++ centralized/ec2_instances.tf
@@ -1,13 +1,3 @@
-module "key_pair" {
-  source = "terraform-aws-modules/key-pair/aws"
-
-  key_name   = "${var.environment}-sharedservices-ssh-keypair"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQD0YgrVVIcvqxp34XSZPTBRYas6MGIlcCyZVzs/BmXpxl/3N+KB33VEfWguTWcXwhQun/J8/hjiEOqrknem91D9qfO/PfrLV/zeE05zKJEgmgJPjJaf2TYRFUZzTZyNmj2p2uPe7/12XhP5JqF7F4lk65eP1hdqLw51JauIxyt3yUS6DaHqSzdbWbrOqkQvpQrY2s/6CnDbLHLzRpnNnbJ5ORkM50obCuoy84C/JvAbrZm3sHTu+TXAvxcTXO5tCPK3i7peUdlnGHKN1Na+jln9xkMscgeSwHM1l/Oh0/YpM/88MMgRQvvYhd/944WRgH/WYknsQQDm83t14jUYhR7JNe0HeuaW9pHFCGLuuOshrKEaV9SPb/2324mnkTzwMGzlVLYTGLqM55NWc57x5BQK1obESM9RlGZA1bPkX5HRg/9Vraes+D8BB7CYiDrdIJlR+Q3t551vgyI9ygiIZmvtHdNQJk6Ww5XY3GbQZmaOjq1oyFZPhq1xB2gvMUVONnM= mukesh.sharma@Mukesh-Sharmas-Macbook-Pro.local"
-
-}
-
-
-
 module "spoke_vpc_a_ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 2.0"
