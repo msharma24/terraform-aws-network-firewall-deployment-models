e0226cb0859c7674a42fd2419c12b634ffeb75af
Mukesh Sharma <mukeshsharma24@gmail.com>
2021-08-26 15:00:19 +1200

    Updated ec2 config

--- centralized/ec2_instances.tf
+++ centralized/ec2_instances.tf
@@ -18,25 +18,6 @@ module "spoke_vpc_a_ec2_instance" {
   }
 }
 
-module "spoke_vpc_a_ec2_instance_2" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 2.0"
-
-  name           = "${var.environment}/spoke_vpc_a_instance_2"
-  instance_count = 1
-
-  ami                    = data.aws_ami.amazon-linux-2.id
-  instance_type          = "t2.micro"
-  monitoring             = true
-  vpc_security_group_ids = [module.spoke_vpc_a_ssh_sg.security_group_id]
-  subnet_id              = module.spoke_vpc_a.private_subnets[1]
-  iam_instance_profile   = aws_iam_instance_profile.subnet_instance_iam_profile.id
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
 
 module "spoke_vpc_b_ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
@@ -57,47 +38,5 @@ module "spoke_vpc_b_ec2_instance" {
     Environment = "dev"
   }
 }
-
-
-
-module "spoke_vpc_a_ec2_public_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 2.0"
-
-  name           = "${var.environment}/spoke_vpc_a_public_instance"
-  instance_count = 1
-
-  ami                    = data.aws_ami.amazon-linux-2.id
-  instance_type          = "t2.micro"
-  monitoring             = true
-  vpc_security_group_ids = [module.spoke_vpc_a_ssh_sg.security_group_id]
-  subnet_id              = module.spoke_vpc_a.public_subnets[0]
-  iam_instance_profile   = aws_iam_instance_profile.subnet_instance_iam_profile.id
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
-
-module "spoke_vpc_b_ec2_public_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 2.0"
-
-  name           = "${var.environment}/spoke_vpc_b_public_instance"
-  instance_count = 1
-
-  ami                    = data.aws_ami.amazon-linux-2.id
-  instance_type          = "t2.micro"
-  monitoring             = true
-  vpc_security_group_ids = [module.spoke_vpc_b_ssh_sg.security_group_id]
-  subnet_id              = module.spoke_vpc_b.public_subnets[0]
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
-
 
 
